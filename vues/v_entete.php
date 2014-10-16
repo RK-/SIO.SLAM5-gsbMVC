57b19e3f621a71dc8114f21042502c1f6d36457b
RK <ruby.kevin57@gmail.com>
2014-10-16 21:20:23 +0200

    Feuille de style
    Feuille de style en cours de création avec SASS et Compass

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,15 +1,20 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-  <head>
-    <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
-    <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
-  </head>
-  <body>
-    <div id="page">
-      <div id="entete">
-        <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
-        <h1>Suivi du remboursement des frais</h1>
-      </div>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="UTF-8" />
+    <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title> 
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="./stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="./stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+        <link href="./stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+</head>
+<body>
+    <div id="header">
+        <div id="logo">GSB-FRAIS</div>
+<!--        <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+        <h1>Suivi du remboursement des frais</h1>-->
+    </div>
