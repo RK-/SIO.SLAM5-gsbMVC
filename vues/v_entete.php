824311a28b01ccebdaf61db87fac6e73e99033b9
RK <ruby.kevin57@gmail.com>
2014-10-17 08:44:36 +0200

    changements

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -6,15 +6,15 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="./stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-    <link href="./stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <link href="./css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="./css/print.css" media="print" rel="stylesheet" type="text/css" />
     <!--[if IE]>
-        <link href="./stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <link href="./css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <![endif]-->
 </head>
 <body>
     <div id="header">
         <div id="logo">GSB-FRAIS</div>
-<!--        <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+<!--        <img src="./img/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
         <h1>Suivi du remboursement des frais</h1>-->
     </div>
