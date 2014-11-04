3b9a95f4c44bcbe86e7764714dad02334ac03ed3
RK <ruby.kevin57@gmail.com>
2014-11-04 19:28:26 +0100

    Sass remis en forme à terminer

--- config.rb
+++ config.rb
@@ -1,3 +1,4 @@
+require 'bootstrap-sass'
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
@@ -12,10 +13,10 @@ javascripts_dir = "js"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
